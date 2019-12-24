2d81961a61bbbd85562944f61671713170972c6b
zarakM <mughalzarak@gmail.com>
2019-12-24 13:20:09 -0800

    class done

--- Rounding Number/index.js
+++ Rounding Number/index.js
@@ -1,9 +1,53 @@
 // var answer = 5 / 3;
 // console.log(answer);
+// var roundOf = Math.floor(0.999999999999);
+// console.log(roundOf);
+
+// var decimalNumber = Math.random()
+// var randomNum = (decimalNumber * 5) + 1
+// var roundOf = Math.floor(randomNum)
+// console.log(roundOf)
+
+// console.log(Math.floor((Math.random() * 5) + 1))
+
+// var a = prompt("enter your age")
+// var age = 18
+// var convert = parseInt(a)
+// if(convert === age ){
+//     alert("you are eligible")
+// }else{
+//     alert("you are not eligible")
+// }
+// console.log("PARSE INTEGER",parseInt(1.9887))
+// console.log("PARSE FLOAT", parseFloat(1.9887))
+
+// var a = "200" - "50"
+// console.log(a)
+
+// var num = "20.343"
+// var result = Number(num)
+// console.log(typeof(result))
+// console.log(result)
+
+// var someString = "Islamabad is the capital of Pakistan"
+// var result = someString.indexOf("the")
+// console.log(result)
+
+// var obj = [{
+//     name: "asadikhlas",
+//     age: 23
+// },
+// {
+//     name: "ikhlas",
+//     age: 22
+// }
+// ]
+
+// clvar result = obj.indexOf()
+// console.log(result)
 
 
-var roundOf = Math.floor(0.999999999999);
-console.log(roundOf);
-
-
+// var someString = "Islamabad is the capital the of the Pakistan"
+// var result = someString.indexOf("the")
+// console.log(result)
 
